1f2c248c7433829f3b53c139bb5d0cb686045cfc
lg <abbix@riseup.net>
2025-01-06 15:33:33 -0500

    remove duplicate headers

--- include/atlas/atlas/impl.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <source_location>
-
-namespace Atlas::Impl {
-
-[[noreturn]] void panic(const char *message,
-                        const std::source_location location);
-
-}
